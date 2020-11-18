92907458d13015ccd68fd253dd3b3c308a2fe6a0
Kevin961312 <kevin961312@gmail.com>
2020-11-17 20:00:02 -0500

    Scott Allen

---
+++ .Net Framework/DetectPangram/DetectPangram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DetectPangram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            var result = program.IsPangram("casa!");
+            Console.WriteLine(result);
+        }
+        public bool IsPangram(string str)
+        {
+            var result = str.ToLower().Where(x => Char.IsLetter(x)).Distinct().Count()==26;
+            return result;
+        }
+    }
+}
---
+++ .Net Framework/Features/Features/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> square = x=>x*x;
+            Func<int, int, int> add = (x, y) => x + y;
+            Action<int> write = x => Console.WriteLine(x); 
+            write(square(add(3,5)));
+            IEnumerable<Employee> developers = new Employee[]
+            {
+                new Employee { Id = 1, Name = "Scott" },
+                new Employee { Id = 2, Name = "Chris" },
+            };
+
+            IEnumerable<Employee> sales = new List<Employee>()
+            {
+                new Employee { Id = 3, Name = "Alex"}
+            };
+            //Console.WriteLine(developers.Count());
+            //IEnumerator<Employee> enumerator = developers.GetEnumerator();
+            //while (enumerator.MoveNext())
+            //{
+            //    Console.WriteLine(enumerator.Current.Name);
+            //}
+            var query = developers.Where(x => x.Name.Length == 5).OrderBy(x => x.Name);
+            var query2 = from developer in developers where developer.Name.Length == 5
+                         orderby developer.Name
+                         select developer ;
+            foreach (var employee in query)
+            {
+                Console.WriteLine(employee.Name);
+            }
+         }
+    }
+}
