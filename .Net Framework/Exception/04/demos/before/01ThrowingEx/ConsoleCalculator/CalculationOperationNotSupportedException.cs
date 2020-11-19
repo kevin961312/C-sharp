45fe89cce485c1754438dc3e0695a4f93ea6f8ff
Kevin961312 <kevin961312@gmail.com>
2020-11-19 16:25:50 -0500

    curso excepciones

---
+++ .Net Framework/Exception/04/demos/before/01ThrowingEx/ConsoleCalculator/CalculationOperationNotSupportedException.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+namespace ConsoleCalculator
+{
+    public class CalculationOperationNotSupportedException : CalculationException
+    {
+        public string Operation {get;}
+
+        public CalculationOperationNotSupportedException():
+            base("Specified operation was out of the range of valid values.")
+        {
+                
+        }
+        public CalculationOperationNotSupportedException(string operation)
+            : this()
+        {
+            Operation = operation;
+        }
+        public CalculationOperationNotSupportedException(string operation, string message):
+            base(message)
+        {
+            Operation = operation;
+        }
+        public override string Message
+        {
+            get 
+            {
+                string message = base.Message;
+                if (Operation != null) 
+                {
+                    return message + Environment.NewLine + $" Unsupported operation: {Operation}";
+                }
+                return message;
+            }
+        }
+    }
+}
---
+++ .Net Framework/Exception/04/demos/before/01ThrowingEx/ConsoleCalculator/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleCalculator
+{
+    public class Calculator
+    {
+        public int Calculate(int number1, int number2, string operation)
+        {
+         
+            string nonNullOperation = operation ??
+                 throw new ArgumentNullException(nameof(operation));
+         
+            if (operation == "/")
+            {
+                try
+                {
+                    return Divide(number1, number2);
+                }
+                catch (ArithmeticException ex)
+                {
+                    throw new CalculationException("an error ocurred during calculation ", ex);
+                }
+
+            }
+            else
+            {
+                throw new CalculationOperationNotSupportedException(operation);
+              
+            }
+        }
+        
+        private int Divide(int number, int divisor)
+        {
+            return number / divisor;
+        }
+    }
+}
+
