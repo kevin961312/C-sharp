9aad868501bbd43077c0dcdf31685f04abf02602
Kevin Sebastian Pineda Jaramillo <kevin.pineda@finac.local>
2020-12-03 17:17:28 -0500

    pasar nuevo pc

---
+++ .Net Framework/DoubleQuotes/DoubleQuotes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleQuotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<?xml version=\"1.0\" encoding='utf-16'?><string>C:\\Temp\\Reportes Circ016\\</string>");
+        }
+    }
+}
---
+++ .Net Framework/RegularDemostration/RegularDemostration/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularDemostration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var patterns = new List<string> { "a*b", "a+b", "a?b" };
+            var inputs  =  new List <string> { "a", "b", "ab", "aab", "abb"};
+            patterns.ForEach(pattern =>
+            {
+                Console.WriteLine("Regular expression: {0}", pattern);
+                var regex = new Regex(pattern);
+                inputs.ForEach(input =>
+                {
+                    Console.WriteLine("\tInput pattern: {0}", input);
+                    var results = regex.Matches(input);
+                    if(results.Count <= 0)
+                        Console.WriteLine("\t\tNo Marches Found");
+                    foreach(Match result in results)
+                        Console.WriteLine("\t\t Match found at index {0}. Length: {1}.", result.Index,result.Length);
+                });
+            });
+            Console.ReadKey();
+        }
+    }
+}
